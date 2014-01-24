78473b999fdc0d0af9017b54b0ea18fb7aa180b1
benpruitt <bpruittvt@gmail.com>
2014-01-24 00:30:38 -0500

    added tm tool; reorg of DNAtools

---
+++ DNAtools/tm.py
@@ -0,0 +1,95 @@
+import math
+
+import sublime
+
+from util import *
+
+
+R = 1.9872e-3  # Gas constant (1 / kcal * mol)
+KELVIN = 273.15
+
+
+def divalent_to_monovalent(divalent, dntp):
+    if divalent == 0:
+        dntp = 0
+    if divalent < dntp:
+        divalent = dntp
+    return 120 * math.sqrt(divalent-dntp)
+
+
+def calc_thermo(seq, conc_nm=50, monovalent=50, divalent=0.01, dntp=0.0):
+    ''' Return the thermo parameters for DNA under specified salt cond. 
+
+    '''
+
+    enthalpies = {
+        'AA': 79, 'AT': 72, 'AG': 78, 'AC': 84,
+        'TA': 72, 'TT': 79, 'TG': 85, 'TC': 82,
+        'GA': 82, 'GT': 84, 'GG': 80, 'GC': 98,
+        'CA': 85, 'CT': 78, 'CG': 106, 'CC': 80
+    }
+    entropies = { 
+        'AA': 222, 'AT': 204, 'AG': 210, 'AC': 224,
+        'TA': 213, 'TT': 222, 'TG': 227, 'TC': 222,
+        'GA': 222, 'GT': 224, 'GG': 199, 'GC': 244,
+        'CA': 227, 'CT': 210, 'CG': 272, 'CC': 199
+    }
+    dH = dS = 0
+    # Calculate oligo symmetry
+    sym = seq == reverse_complement(seq, 'DNA')
+    # Calculate NN uncorrected dS and dH for oligo
+    for idx in range(len(seq)-1):
+        dH += enthalpies[seq[idx:idx+2]] 
+        dS += entropies[seq[idx:idx+2]]
+    # Terminal AT penalty and initiation parameters (combined)
+    if seq[0] in 'AT':
+        dH += -23
+        dS += -41
+    else:
+        dH += -1
+        dS += 28
+    if seq[-1] in 'AT':
+        dH += -23
+        dS += -41
+    else:
+        dH += -1
+        dS += 28
+    if sym:
+        dS += 14
+    dH *= -100.0
+    dS *= -0.1
+    # Convert divalent salt and dntp conc. to monovalent equivalencies
+    monovalent += divalent_to_monovalent(divalent, dntp)
+    dS = dS + 0.368 * (len(seq) - 1) * math.log(monovalent / 1000.0)
+    # Account for oligo symmetry and calculate tm
+    if sym:
+        tm = dH / (dS + 1.987 * math.log(conc_nm/1.0e9)) - KELVIN
+    else:
+        tm = dH / (dS + 1.987 * math.log(conc_nm/4.0e9)) - KELVIN
+    return dH, dS, tm
+
+
+def calc_tm(seq, conc_nm=50, monovalent=50, divalent=0.01, dntp=0.0):
+    _, _, tm = calc_thermo(seq, conc_nm=50, monovalent=50, divalent=0.01, 
+                           dntp=0.0)
+    return tm
+
+
+class NaTmDefaultsCommand(NaBase):
+    
+    def process_seq(self, seq, na_type):
+        if na_type == 'RNA':
+            seq = seq.translate(_RNA_TO_DNA)
+        return calc_tm(seq)
+
+
+    def handle_output(self, region, tm):
+        sublime.message_dialog('Tm = {0}\n'.format(tm)        +
+                               '-' * 30 + '\n'                +
+                               'DNA []: \t\t\t50 nM\n'        +
+                               'Monovalent []: \t50 mM\n'     +
+                               'Divalent []: \t\t0.01 mM\n'   +
+                               'dNTP []: \t\t0 mM')
+                               
+
+    
---
+++ DNAtools/util.py
@@ -0,0 +1,81 @@
+import re
+import sublime
+import sublime_plugin
+try:
+    from string import maketrans
+except:
+    from str import maketrans
+
+
+__all__ = ['_DNA_COMP', '_RNA_COMP', '_NA_RE', '_DNA_RE', '_RNA_RE',
+           '_RNA_TO_DNA', 'seqQC', 'complement', 'reverse_complement', 
+           'reverse', 'NaBase']
+
+
+_DNA_COMP = maketrans('ATGCatgcKMRYSWBVHDXN.- ', 
+                      'TACGtacgMKYRSWVBDHXN.- ')
+
+_RNA_COMP = maketrans('AUGCaugcKMRYSWBVHDXN.- ', 
+                      'UACGuacgMKYRSWVBDHXN.- ')
+
+_RNA_TO_DNA = maketrans('Uu','Tt')
+
+_NA_RE = re.compile('[ATGCUatgcuKMRYSWBVHDXN\.\- ]+')
+_DNA_RE = re.compile('[ATGCatgcKMRYSWBVHDXN\.\- ]+')
+_RNA_RE = re.compile('[AUGCaugcKMRYSWBVHDXN\.\- ]+')
+
+
+def seqQC(seq):
+    if re.match(_DNA_RE, seq):
+        return 'DNA'
+    if re.match(_RNA_RE, seq):
+        return 'RNA'
+    else:
+        return None
+
+
+def complement(seq, na_type):
+    trans = _DNA_COMP if na_type == 'DNA' else _RNA_COMP
+    return seq.translate(trans)
+
+
+def reverse_complement(seq, na_type):
+    return complement(seq, na_type)[::-1]
+
+
+def reverse(seq, na_type):
+    return seq[::-1]
+
+
+class NaBase(sublime_plugin.TextCommand):
+    ''' Base class for Na commands. Override process_seq and handle_output
+        for individual subclasses.
+    '''
+
+    def run(self, edit):
+        region = self.view.sel()[0]
+        seq = str(self.view.substr(region).strip())
+        na_type = seqQC(seq)
+        if not na_type:
+            sublime.error_message('Selected text contains invalid DNA/RNA ' + 
+                                  'bases. Valid bases include: \n\n' +
+                                  'DNA: ATGCatgcKMRYSWBVHDXN.- \n' +
+                                  'RNA: AUGCaugcKMRYSWBVHDXN.-')
+        else:
+            seq_out = self.process_seq(seq, na_type)
+            self.handle_output(region, seq_out)
+
+
+    def handle_output(self, region, seq_out):
+        pass
+
+    def process_seq(self, seq, na_type):
+        return seq
+
+    def is_enabled(self):
+        region = self.view.sel()[0]
+        seq = self.view.substr(region).strip()
+        if re.match(_NA_RE, seq):
+            return True
+        else:
+            return False
